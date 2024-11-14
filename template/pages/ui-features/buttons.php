95a022dd567199ee3beae77a2a97d46732ce2e3f
SureshGopi1999 <gopisuresh1008@gmail.com>
2024-11-14 20:53:54 +0530

    changed

--- template/pages/ui-features/buttons.php
+++ template/pages/ui-features/buttons.php
@@ -332,30 +332,30 @@
       <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <ul class="nav">
           <li class="nav-item">
-            <a class="nav-link" href="../../index.html">
+            <a class="nav-link" href="../../index.php">
               <i class="typcn typcn-device-desktop menu-icon"></i>
-              <span class="menu-title">Dashboard</span>
+              <span class="menu-title">New Customer</span>
               <div class="badge badge-danger">new</div>
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
               <i class="typcn typcn-document-text menu-icon"></i>
-              <span class="menu-title">UI Elements</span>
+              <span class="menu-title">Interest Loan</span>
               <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="ui-basic">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="../../pages/ui-features/buttons.html">Buttons</a></li>
-                <li class="nav-item"> <a class="nav-link" href="../../pages/ui-features/dropdowns.html">Dropdowns</a></li>
-                <li class="nav-item"> <a class="nav-link" href="../../pages/ui-features/typography.html">Typography</a></li>
+                <li class="nav-item"> <a class="nav-link" href="../../pages/ui-features/buttons.php">Buttons</a></li>
+                <li class="nav-item"> <a class="nav-link" href="../../pages/ui-features/dropdowns.php">Dropdowns</a></li>
+                <li class="nav-item"> <a class="nav-link" href="../../pages/ui-features/typography.php">Typography</a></li>
               </ul>
             </div>
           </li>
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
               <i class="typcn typcn-film menu-icon"></i>
-              <span class="menu-title">Form elements</span>
+              <span class="menu-title">Voucher</span>
               <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="form-elements">
@@ -367,7 +367,7 @@
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
               <i class="typcn typcn-chart-pie-outline menu-icon"></i>
-              <span class="menu-title">Charts</span>
+              <span class="menu-title">Outstanding</span>
               <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="charts">
--- template/partials/_sidebar.php
+++ template/partials/_sidebar.php
@@ -1,4 +1,5 @@
-<nav class="sidebar sidebar-offcanvas" id="sidebar">
+ <!-- partial:partials/_sidebar.html -->
+ <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <ul class="nav">
           <li class="nav-item">
             <a class="nav-link" href="index.php">
@@ -7,24 +8,73 @@
               <div class="badge badge-danger">new</div>
             </a>
           </li>
+
           <li class="nav-item">
+            <a class="nav-link" href="newcustomer.php">
+            <i class="typcn typcn-document-text menu-icon"></i>
+            <span class="menu-title">New Customer</span>
+              <!-- <div class="badge badge-danger">new</div> -->
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="interestloan.php">
+            <i class="typcn typcn-document-text menu-icon"></i>
+            <span class="menu-title">Interest Loan</span>
+              <!-- <div class="badge badge-danger">new</div> -->
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="receiptentry.php">
+            <i class="typcn typcn-document-text menu-icon"></i>
+            <span class="menu-title">Receipt Entry</span>
+              <!-- <div class="badge badge-danger">new</div> -->
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="Outstanding.php">
+            <i class="typcn typcn-document-text menu-icon"></i>
+            <span class="menu-title">Out Standing</span>
+              <!-- <div class="badge badge-danger">new</div> -->
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="closedloan.php">
+            <i class="typcn typcn-document-text menu-icon"></i>
+            <span class="menu-title">closed Loan</span>
+              <!-- <div class="badge badge-danger">new</div> -->
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="accountsreport.php">
+            <i class="typcn typcn-document-text menu-icon"></i>
+            <span class="menu-title">Accounts Reports</span>
+              <!-- <div class="badge badge-danger">new</div> -->
+            </a>
+          </li>
+
+          <!-- <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
               <i class="typcn typcn-document-text menu-icon"></i>
-              <span class="menu-title">NEW CUSTOMER</span>
+              <span class="menu-title">New Customer</span>
               <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="ui-basic">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">1</a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/newcustomer.php">Buttons</a></li>
                 <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
                 <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
               </ul>
-            </div>
-          </li>
+            </div> 
+           </li>
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
               <i class="typcn typcn-film menu-icon"></i>
-              <span class="menu-title">Form elements</span>
+              <span class="menu-title">Interst Loan</span>
               <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="form-elements">
@@ -36,7 +86,7 @@
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
               <i class="typcn typcn-chart-pie-outline menu-icon"></i>
-              <span class="menu-title">Charts</span>
+              <span class="menu-title">Accounts</span>
               <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="charts">
@@ -48,7 +98,7 @@
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
               <i class="typcn typcn-th-small-outline menu-icon"></i>
-              <span class="menu-title">Tables</span>
+              <span class="menu-title">Vouchur</span>
               <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="tables">
@@ -60,7 +110,7 @@
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
               <i class="typcn typcn-compass menu-icon"></i>
-              <span class="menu-title">Icons</span>
+              <span class="menu-title">Outstanding</span>
               <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="icons">
@@ -68,8 +118,8 @@
                 <li class="nav-item"> <a class="nav-link" href="pages/icons/mdi.html">Mdi icons</a></li>
               </ul>
             </div>
-          </li>
-          <li class="nav-item">
+          </li> -->
+          <!-- <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
               <i class="typcn typcn-user-add-outline menu-icon"></i>
               <span class="menu-title">User Pages</span>
@@ -81,8 +131,8 @@
                 <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
               </ul>
             </div>
-          </li>
-          <li class="nav-item">
+          </li> -->
+          <!-- <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#error" aria-expanded="false" aria-controls="error">
               <i class="typcn typcn-globe-outline menu-icon"></i>
               <span class="menu-title">Error pages</span>
@@ -94,12 +144,10 @@
                 <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
               </ul>
             </div>
-          </li>
-          <li class="nav-item">
+          </li> -->
+          <!-- <li class="nav-item">
             <a class="nav-link" href="https://bootstrapdash.com/demo/polluxui-free/docs/documentation.html">
               <i class="typcn typcn-mortar-board menu-icon"></i>
               <span class="menu-title">Documentation</span>
             </a>
-          </li>
-        </ul>
-      </nav>
+          </li> -->
